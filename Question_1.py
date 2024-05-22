ad35bc9b3347f41106c66cd4a1c512f7b6b56b61
samuelckraft <samuelckraft@gmail.com>
2024-05-22 11:09:17 -0400

    update

--- Question_1.py
+++ Question_1.py
@@ -1,1 +1,49 @@
 #Task 1
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+        self.status = True
+
+    def change_title(self, new_title):
+        self.title = new_title
+        
+    def change_author(self, new_author):
+        self.author = new_author
+    
+    def change_price(self, new_price):
+        self.price = new_price
+
+    def change_status(self):
+        self.status = False if True else self.status == True
+
+    def display_book(self):
+        if self.status:
+            self.status = 'Yes'
+        else:
+            self.status = 'No'
+        print(f"Title: {self.title}\nAuthor: {self.author}\nPrice: {self.price:.2f}\nIn Stock: {self.status}")
+
+
+
+book1 = Book('Red Rising', 'Pierce Brown', 25.99)
+
+book1.display_book()
+    #Output: 
+    #Title: Red Rising
+    #Author: Pierce Brown
+    #Price: 25.99
+    #In Stock: Yes
+
+book1.change_title('Fellowship of the Ring')
+book1.change_author('JRR Tolkien')
+book1.change_price(30.99)
+book1.change_status()
+
+book1.display_book()
+    #Output:
+    # Title: Fellowship of the Ring
+    # Author: JRR Tolkien
+    # Price: 30.99
+    # In Stock: No
--- Question_2.py
+++ Question_2.py
@@ -1,1 +1,30 @@
-#Task 2
+#Task 1
+class Weather:
+    def __init__(self, city, temperature, condition, humidity):
+        self.city = city
+        self.temperature = temperature
+        self.condition = condition
+        self.humidity = humidity
+
+    def parse_weather_data(self):
+        return f"Weather in {self.city}: {self.temperature} degrees, {self.condition}, Humidity: {self.humidity}%"
+
+    def display_weather(self):
+        print(weather_data[self.city])
+
+
+
+
+
+weather_data = {
+        "New York": {"temperature": 70, "condition": "Sunny", "humidity": 50},
+        "London": {"temperature": 60, "condition": "Cloudy", "humidity": 65},
+        "Tokyo": {"temperature": 75, "condition": "Rainy", "humidity": 70}
+    }
+
+NY_weather = Weather('New York', weather_data["New York"]['temperature'], weather_data["New York"]['condition'], weather_data["New York"]['humidity'])
+London_weather = Weather('London', weather_data["London"]['temperature'], weather_data["London"]['condition'], weather_data["London"]['humidity'])
+Tokyo_weather = Weather('Tokyo', weather_data["Tokyo"]['temperature'], weather_data["Tokyo"]['condition'], weather_data["Tokyo"]['humidity'])
+
+NY_weather.parse_weather_data()
+NY_weather.display_weather()
